cdf59293bb6603daead49e9d642786ca30c341bb
Freeman4pm <38683598+Freeman4pm@users.noreply.github.com>
2020-04-17 16:20:08 +0200

    Add files via upload

---
+++ db_connection.py
@@ -0,0 +1,112 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Mar 31 12:39:51 2020
+
+@author: hanshow
+"""
+
+from sqlalchemy import create_engine
+import pymysql
+from datetime import date
+
+database_host = 'localhost'
+database_port = '3306'
+username = "root"
+password = "admin"
+database_name = "esl_lowbattary"
+table_name = 'default'
+
+def data_to_db(database_host, database_port, username, password, 
+               database_name, table_name, df):
+    db_data = 'mysql+mysqldb://' + username + ':' + password + '@' + database_host + ':'+ database_port + '/' \
+           + database_name + '?charset=utf8'
+    engine = create_engine(db_data)
+    
+    df.to_sql(table_name, engine, if_exists='append', index=False)
+    
+    engine.dispose()
+
+def create_new_db(database_host, database_port, username, password, 
+                  database_name, table_name, data_type = 'esl'):
+    conn = pymysql.connect(host=database_host, port=int(database_port), 
+                           user=username, passwd=password, db=database_name)
+    cursor = conn.cursor()
+    exist = cursor.execute("show tables like '%s'" % table_name)
+    if exist:
+        cursor.execute("drop table %s" % table_name)
+        print("The table already exists! Old table has been dropped.")
+    cursor.execute("CREATE TABLE IF NOT EXISTS " + table_name + ' ' + "(LIKE dm____struct_{}_all)".format(data_type))
+    conn.commit()
+    cursor.close()
+    conn.close()  
+    return exist
+def drop_duplicates_db(database_host, database_port, username, password, 
+                  database_name, table_name):
+    conn = pymysql.connect(host=database_host, port=int(database_port), 
+                           user=username, passwd=password, db=database_name)
+    cursor = conn.cursor()
+    sql = "delete from {} where auto_id not in \
+             (select auto_id from \
+              (SELECT min(auto_id), eslid From {} \
+               group by eslid) t ) ".format(table_name, table_name)
+    dropped = cursor.execute(sql)
+    conn.commit()
+    cursor.close()
+    conn.close()
+    return dropped
+
+def drop_duplicates_db_2(database_host, database_port, username, password, 
+                  database_name, table_name_s1, table_name_s3):
+    conn = pymysql.connect(host=database_host, port=int(database_port), 
+                           user=username, passwd=password, db=database_name)
+    cursor = conn.cursor()
+    sql = "SELECT REPLACE(GROUP_CONCAT(COLUMN_NAME), 'auto_id,', '')\
+    FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME = '{}' \
+    AND TABLE_SCHEMA = '{}'".format(table_name_s1, database_name)
+    cursor.execute(sql)
+    column_list = cursor.fetchall()
+    sql = "INSERT INTO {} ({}) SELECT {} FROM {} th where not exists\
+                   (select 1 from {} where esl_id = th.esl_id\
+                    and esl_create_date>th.esl_create_date) ".format(table_name_s3,column_list[0][0], column_list[0][0], table_name_s1, table_name_s1)
+    cursor.execute(sql)
+    conn.commit()
+    cursor.close()
+    conn.close()
+
+def db_table_list(database_host, database_port, username, password, 
+                  database_name, tag, data_type = 'esl'):
+    conn = pymysql.connect(host=database_host, port=int(database_port), 
+                           user=username, passwd=password, db=database_name)
+    cursor = conn.cursor()
+    cursor.execute("SELECT TABLE_NAME \
+                   FROM INFORMATION_SCHEMA.TABLES\
+                   WHERE TABLE_TYPE = 'BASE TABLE' \
+                   AND TABLE_SCHEMA = '{}' AND TABLE_NAME LIKE 's0%{}%{}%'".format(database_name,tag, data_type))
+    table_list = cursor.fetchall()
+    cursor.close()
+    conn.close()
+    return table_list
+
+def merge_tables(database_host, database_port, username, password, 
+                  database_name, table_list, s3_table):
+    conn = pymysql.connect(host=database_host, port=int(database_port), 
+                           user=username, passwd=password, db=database_name)
+    cursor = conn.cursor()
+    sql = "SELECT REPLACE(GROUP_CONCAT(COLUMN_NAME), 'auto_id,', '')\
+    FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME = '{}' \
+    AND TABLE_SCHEMA = '{}'".format(s3_table, database_name)
+    cursor.execute(sql)
+    column_list = cursor.fetchall()
+    for table_name in table_list:
+        sql = "INSERT INTO {} ({}) SELECT {} FROM {}".format(s3_table, column_list[0][0], column_list[0][0], table_name)
+        cursor.execute(sql)
+        conn.commit()
+    cursor.close()
+    conn.close()
+        
+if __name__ == "__main__":
+#    data_to_db(database_host, database_port, username, password, 
+#               database_name, table_name)
+#    create_new_db(database_host, database_port, username, password, 
+#                  database_name, 'default2')
+    pass
---
+++ main_esl.py
@@ -0,0 +1,121 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Mar 24 20:42:56 2020
+
+@author: hanshow
+"""
+
+import chardet
+import os
+from datetime import date
+import pandas as pd
+import db_connection
+import json 
+
+###############################Import setting file#############################
+with open("setting.json",'r', encoding = 'utf-8') as load_f:
+    settings = json.load(load_f)
+    
+path = settings['scandata']['eslpath']
+database_host = settings['conn']['ip']
+database_port = settings['conn']['port']
+username = settings['conn']['uname']
+password = settings['conn']['pwd']
+database_name = settings['conn']['db']
+merge_tags = settings['mergedata']['fromtag']
+merge_table = settings['mergedata']['totag']
+
+
+def get_encoding(file):
+    # 二进制方式读取，获取字节数据，检测类型
+    with open(file, 'rb') as f:
+        return chardet.detect(f.read())['encoding']
+
+###############################SCANDATA#######################################
+g = os.walk(path)
+time_stamp = date.today().strftime("%y%m%d")
+print("================Scan data==================")
+for path,dir_list,file_list in g:  
+    if len(file_list) == 0:
+        continue
+    df = pd.DataFrame()
+    tag = [x.split('-')[2] for x in file_list]
+    tag = list(set(tag))
+    if len(tag) > 1:
+        raise Exception('CustomerError: The folder' + path + 'contains more than one customer data') 
+    tag = tag[0]
+    for file_name in file_list:
+        full_path = os.path.join(path,file_name)
+        print("converting codec {}".format(full_path))
+        encoding = get_encoding(full_path)
+        if encoding[:3] != 'UTF' and encoding[:3] != 'utf':
+            dft = pd.read_csv(full_path,encoding = 'GBK')
+        else:
+            dft = pd.read_csv(full_path,encoding = encoding)
+        dft['tag_customer'] = tag
+        dft['tag_create_date'] = pd.to_datetime(dft['tag_create_date'], format = "%d/%m/%Y %H:%M:%S")
+        dft['esl_create_date'] = pd.to_datetime(dft['esl_create_date'], format = "%d/%m/%Y %H:%M:%S")
+        dft['esl_battery_date'] = pd.to_datetime(dft['esl_battery_date'], format = "%d/%m/%Y %H:%M:%S")
+    
+        df = pd.concat([df,dft])
+    s0_table_name = 's0___' + tag + '_' + 'esl' + '_' + time_stamp
+    s3_table_name = 's3___' + tag + '_' + 'esl' + '_' + time_stamp
+    db_connection.create_new_db(database_host, database_port, username, password, 
+                  database_name, s0_table_name)
+
+    db_connection.data_to_db(database_host, database_port, username, password, 
+                   database_name, s0_table_name, df) 
+    print("Data reading complete for folder " + path) 
+    
+    df['esl_id'].to_csv('esl_id/esl_id_{}_{}.csv'.format(tag, time_stamp),mode = 'w', encoding = 'GBK', index = False)
+    
+    db_connection.create_new_db(database_host, database_port, username, password, 
+                  database_name, s3_table_name)
+#    db_connection.data_to_db(database_host, database_port, username, password, 
+#               database_name, 's3___'+tag+'_'+time_stamp, df) 
+#
+#    dropped = db_connection.drop_duplicates_db(database_host, database_port, username, password, 
+#                  database_name, 's3___'+tag+'_'+time_stamp)
+    db_connection.drop_duplicates_db_2(database_host, database_port, username, password, 
+                  database_name, s0_table_name, s3_table_name)
+    print("Duplicated eslids dropped for {}".format(tag))
+###########################MERGEDATA###########################################
+print("================Merge data==================")
+table_list = []
+s2_table_name = 's2___' + merge_table + '_' + 'esl' + '_' + time_stamp + '_' + 'merged'
+s3_table_name = 's3___' + merge_table + '_' + 'esl' + '_' + time_stamp + '_' + 'merged'
+for tag in merge_tags:
+    table_tuple = db_connection.db_table_list(database_host, database_port, 
+                                              username, password, database_name, tag)
+    for row in table_tuple:
+        table_list.append(row[0])
+table_list = list(set(table_list))
+db_connection.create_new_db(database_host, database_port, username, password, 
+                  database_name, s2_table_name)
+db_connection.merge_tables(database_host, database_port, username, password, 
+                           database_name, table_list, s2_table_name)
+db_connection.create_new_db(database_host, database_port, username, password, 
+                  database_name, s3_table_name)
+db_connection.drop_duplicates_db_2(database_host, database_port, username, password, 
+                  database_name, s2_table_name, s3_table_name)
+print('Data Merge Completed')
+#dropped = db_connection.drop_duplicates_db(database_host, database_port, username, password, 
+#                  database_name, 's3___'+merge_table+'_'+time_stamp+'_'+'merged')
+#print("{} Duplicated eslids dropped".format(dropped))
+
+######################数据去重并将s3表输入数据库###################################
+#df_l1 = df.shape[0]
+##df.drop_duplicates('esl_id', 'first', inplace = True)
+##df_l2 = df.shape[0]
+##print("{} Duplicated eslids dropped".format(df_l1-df_l2))
+#
+#######################保存合并后的表以及提取出的eslid###############################
+#if df_l1 != 0:
+#    df.to_csv('esl_data.csv',mode = 'a', encoding = 'GBK', index = False)
+#    df['esl_id'].to_csv('esl_id.csv',mode = 'a', encoding = 'GBK', index = False)
+print("Finished")
+
+    
+
+    
+
